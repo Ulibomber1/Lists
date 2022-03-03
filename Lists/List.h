b87b278a6bcd8039fc2657de0c00edd839452032
Arius <ulibomber1@gmail.com>
2022-03-02 20:09:59 -0800

    Copied code from instructor's slides. Built successfully, but has not been tested for logical errors yet.

--- Lists/List.h
+++ Lists/List.h
@@ -20,18 +20,246 @@ private:
 		Node(const T & d = T{}, Node * p = nullptr, Node * n = nullptr)
 			: data{d}, prev{p}, next{n} { }
 	};
+public:
+	class const_iterator 
+	{
+	public:
+		// Public constructor for const_iterator.
+		const_iterator() : current{ nullptr }
+		{}
+
+		const T& operator* () const
+		{
+			return retrieve();
+		}
+
+		// prefix increment
+		const_iterator& operator++ ()
+		{
+			current = current->next;
+			return *this;
+		}
+
+		// postfix increment
+		const_iterator operator++ (int)
+		{
+			const_iterator old = *this;
+			++(*this);
+			return old;
+		}
+
+		const_iterator& operator-- ()
+		{
+			current = current->prev;
+			return *this;
+		}
+
+		const_iterator operator-- (int)
+		{
+			const_iterator old = *this;
+			--(*this);
+			return old;
+		}
+
+		bool operator== (const const_iterator& rhs) const
+		{
+			return current ++ rhs.current;
+		}
+
+		bool operator!= (const const_iterator& rhs) const
+		{
+			return !(*this == rhs);
+		}
+
+	protected:
+		Node* current;
+		T& retrieve() const
+		{
+			return current->data;
+		}
+
+		const_iterator(Node* p) : current{ p } { }
+
+		friend class List<T>;
+	};
+
+	class iterator : public const_iterator
+	{
+	public:
+		iterator() {}
+
+		T & operator* ()
+		{
+			return const_iterator::retrieve();
+		}
+
+		const T & operator* () const
+		{
+			return const_iterator::operator*();
+		}
+
+		iterator& operator++ ()
+		{
+			this->current = this->current->next;
+			return *this;
+		}
+
+		iterator operator++ (int)
+		{
+			iterator old = *this;
+			++(*this);
+			return old;
+		}
+
+		iterator& operator-- ()
+		{
+			this->current = this->current->prev;
+			return *this;
+		}
+
+		iterator operator-- (int)
+		{
+			iterator old = *this;
+			--(*this);
+			return old;
+		}
+
+	protected:
+		iterator(Node *p) : const_iterator{p} { }
+
+		friend class List<T>;
+	};
 
 public:
-	// Public constructor for const_iterator.
-	const_iterator( ) : current{nullptr}
-	{}
-
-	const T & operator* () const
+	List()
 	{
-		return retrieve();
+		init();
 	}
 
-	const_iterator & operator
+	~List()
+	{
+		clear();
+		delete head;
+		delete tail;
+	}
+
+	List(const List& rhs)
+	{
+		init();
+		/*KV's cut...
+		for (auto & x : rhs)
+			push_back(x);
+		*/
+		// more generic:
+		cosnt_iterator itr = rhs.begin();
+		for (; itr != rhs.end(); ++itr)
+			push_back(*itr);
+	}
+
+	List & operator= (const List& rhs)
+	{
+		List copy = rhs;
+		std::swap(*this, copy);
+		return *this;
+	}
+
+	// Return iterator representing beginning of list.
+	// Mutator version is first, then accessor version.
+	iterator begin()
+	{
+		return iterator(head->next);
+	}
+
+	const_iterator begin() const
+	{
+		return const_iterator(head->next);
+	}
+
+	// Return iterator representing endmarker of list.
+	// Mutator version is first, then accessor version.
+	iterator end()
+	{
+		return iterator(tail);
+	}
+
+	const_iterator end() const
+	{
+		return const_iterator(tail);
+	}
+
+	// Return number of elements currently in the list.
+	int size() const
+	{
+		return theSize;
+	}
+
+	// Return true if the list is empty, false otherwise.
+	bool empty() const
+	{
+		return size() == 0;
+	}
+
+	void clear()
+	{
+		while (!empty())
+			pop_front();
+	}
+
+	// front, back, push_front, push_back, pop_front, and pop_back
+	// are the basic double-ended queue operations.
+	T& front()
+	{
+		return *begin();
+	}
+
+	const T& front() const
+	{
+		return *begin();
+	}
+
+	T& back()
+	{
+		return *--end();
+	}
+	
+	const T& back() const
+	{
+		return *--end();
+	}
+
+	void push_front(const T& x)
+	{
+		insert(begin(), x);
+	}
+
+	void push_back(const T& x)
+	{
+		insert(end(), x);
+	}
+
+	void pop_front()
+	{
+		erase(begin());
+	}
+
+	void pop_back()
+	{
+		erase(--end());
+	}
+
+private:
+	int theSize;
+	Node* head;
+	Node* tail;
+
+	void init()
+	{
+		theSize = 0;
+		head = new Node;
+		tail = new Node;
+		head->next = tail;
+		tail->prev = head;
+	}
 };
 
 #endif
