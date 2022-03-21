91855ad052d724041f3b19e0d194979efebc23b5
Arius <ulibomber1@gmail.com>
2022-03-21 12:31:29 -0700

    Added the algorithms to Searching.h and added entirety of Lab9Main.cpp.

---
+++ Lists/Lab9Main.cpp
@@ -0,0 +1,65 @@
+// For CSE 2020 Lab9
+// KV, Spring 2022
+#include <iostream>
+#include <algorithm>
+#include "Vector.h"
+#include "List.h"
+#include "Random.h"
+#include "Searching.h"
+#include "Utilities.h"
+
+using namespace std;
+
+const int RUNS = 20;
+
+int main()
+{
+	rand_seed();
+	for (int k = 100; k <= 1000; k += 100)
+	{
+		Vector<int> myvec;
+		random_vector_norep(k, 1, 10000, myvec, 0);
+
+		Vector<int> sortvec(myvec);
+		sort(sortvec.begin(), sortvec.end());
+
+		List<int> sortlist;
+		for (int i = 0; i < sortvec.size(); i++)
+			sortlist.push_back(sortvec[i]);
+
+		Vector<int> targets;
+		random_vector_norep(RUNS, 0, myvec.size() - 1, targets, 0);
+
+		int sum_lsvec = 0; // initialize sums for averaging ... 
+		int sum_lslst = 0;
+		int sum_lslst2 = 0;
+
+		for (int i = 0; i < targets.size(); i++)
+		{
+			int ops_lsvec = 0;
+			int ops_lslst = 0;
+			int ops_lslst2 = 0;
+
+			int target = myvec[targets[i]]; // next target to search
+
+			linear_search_V(sortvec, target, ops_lsvec);
+			linear_search_L(sortlist, target, ops_lslst);
+			linear_search_L_explore(sortlist, target, ops_lslst2);
+
+
+			sum_lsvec += ops_lsvec;
+			sum_lslst += ops_lslst;
+			sum_lslst2 += ops_lslst2;
+		}
+
+		cout << "Averages of " << RUNS << " runs for size " << k << "....." << endl << endl;
+		cout << "linear_search_V: " << sum_lsvec / float(RUNS) << " avg" << endl;
+		cout << "linear_search_L: " << sum_lslst / float(RUNS) << " avg" << endl;
+		cout << "linear_search_Lexp: " << sum_lslst2 / float(RUNS) << " avg" << endl;
+		cout << endl;
+
+
+
+	}
+	return 0;
+}
--- Lists/Searching.h
+++ Lists/Searching.h
@@ -13,8 +13,14 @@ using namespace std;
 template <typename T>
 int linear_search_V(const Vector<T>& vec, const T& target, int& ops)
 {
-	// Lab9 : Complete as show in lecture Module 6.2
-
+	ops = 0;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		ops++;
+		if (vec[i] == target)
+			return i;
+	}
+	return -1; // not found
 }
 
 // LINEAR SEARCH OF LINKED LIST with iterator 
@@ -24,7 +30,16 @@ int linear_search_V(const Vector<T>& vec, const T& target, int& ops)
 template <typename T>
 typename List<T>::const_iterator linear_search_L(const List<T>& lst, const T& target, int& ops)
 {
-	// Lab9: Complete as show in lecture Module 6.2
+	ops = 0;
+
+	typename List<T>::const_iterator itr;
+	for (itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		ops++;
+		if (*itr == target)
+			return itr;
+	}
+	return lst.end();
 }
 
 // Linear search of List with pseudo-random access [int] operator
@@ -33,7 +48,14 @@ typename List<T>::const_iterator linear_search_L(const List<T>& lst, const T& ta
 template <typename T>
 int linear_search_L_explore(List<T> lst, const T& target, int& ops)
 {
-	// Lab 9: Complete on your own; see lab instructions;  
+	ops = 0;
+	for (int i = 0; i < lst.size; i++)
+	{
+		ops++;
+		if (lst[i] == target)
+			return i;
+	}
+	return -1; // not found
 }
 
 
